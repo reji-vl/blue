916dce0184aaa7bbdf1ef8ea23fa90e5ac2f8bac
reji-vl <56560619+reji-vl@users.noreply.github.com>
2020-02-24 23:13:49 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,4 +1,16 @@
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
+variable "subscription_id" {
+	type="string"
+	default = "https://bluekvreji.vault.azure.net/secrets/subID/e370dfaa2d8e437f95f65a0f30dc13b2"	
+}
+variable "client_id" {
+	type = "string"
+	default = "https://bluekvreji.vault.azure.net/secrets/tenantId/3b39d714dd9d422faca88cfddad3dc36"
+}
+variable "client_secret" {
+	type = "string"
+	default = "https://bluekvreji.vault.azure.net/secrets/clientid/2871d1c95d99487592fdd2972ead3602"
+}
+variable "tenant_id" {
+	type = "string"
+	default = "https://bluekvreji.vault.azure.net/secrets/clipass/dda0c82ec19144af99d2b2b4cab4b41b"
+}
